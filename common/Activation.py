fix formula back prop

common/Activation.py
@@ -1,13 +1,22 @@
 import numpy as np
-from .Logger import Logger
+from .logger import Logger
 
 logger = Logger()
+
+class Activation():
+    def __init__(self, activation, derivate):
+        self.activation = activation
+        self.derivate = derivate
+
+########################################
+#       Funzioni attivazione           #
+########################################
 
 def fun_ReLU(a):
     return np.maximum(a, 0)
 
 
-def Derivata_ReLU(a):
+def derivate_ReLU(a):
     return np.where(a > 0, 1, 0)
 
 
@@ -16,13 +25,13 @@ def LReLU(a):
     return np.maximum(a, alfa * a)
 
 
-def softmax(a):
+def fun_softmax(a):
     a = np.asarray(a)
     exp_a = np.exp(a)
     probs = exp_a / np.sum(exp_a, axis=0, keepdims=True)
     
-    col_sums = np.sum(probs, axis=0)
-    logger.print(col_sums, "softmax col sums:")
+    #col_sums = np.sum(probs, axis=0)
+    #logger.print(col_sums, "softmax col sums")
 
     return probs
     
@@ -31,11 +40,11 @@ def delta_softmax(y,t):
     probs = y-t
     return probs
 
-class Activation():
-    def __init__(self, activation, derivate):
-        self.activation = activation
-        self.derivate = derivate
+########################################
+#               Oggetti                #
+########################################
 
-ReLU = Activation(fun_ReLU,Derivata_ReLU)
 
-Softmax = Activation(softmax,delta_softmax)
+ReLU = Activation(fun_ReLU,derivate_ReLU)
+
+Softmax = Activation(fun_softmax,delta_softmax)

main.py
@@ -1,7 +1,7 @@
 from Model.NeuralNetwork import NeuralNetwork as nn
-from common.Activation import ReLU, softmax
-from common.Weight_Init import Glorot, He
-from common.Loss import CrossEntropy
+from common.activation import ReLU, Softmax
+from common.weight_Init import Glorot, He
+from common.loss import CrossEntropy
 import numpy as np
 from tensorflow.keras.datasets import mnist
 
@@ -22,6 +22,6 @@ x_test  = x_test.reshape(x_test.shape[0], -1)
 rete = nn([784,500,400,10]) # creo la rete
 input=np.atleast_2d(x_train[0:100]) # prendo input che è una matrice
 label=np.atleast_1d(y_train[0:100]) # label di ogni input (è un vettore per ora)
-print(label)
-rete.train(input,label,1,12)
+#print(label)
+rete.train(input,label,1,20)
 #rete.forward(input)